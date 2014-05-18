7525543ac94adfb8deff4a116f2d4f8c909c87ba
hongoctrien <hongoctrien@2mit.org>
2014-05-18 22:48:53 +0700

    Remove config func

--- modules/nvform/admin.functions.php
+++ modules/nvform/admin.functions.php
@@ -13,7 +13,6 @@ if ( ! defined( 'NV_ADMIN' ) or ! defined( 'NV_MAINFILE' ) or ! defined( 'NV_IS_
 $submenu['form_content'] = $lang_module['form_add'];
 $submenu['question'] = $lang_module['question_list'];
 $submenu['question_content'] = $lang_module['question_add'];
-$submenu['config'] = $lang_module['config'];
 
 $allow_func = array( 'main', 'config','form_content', 'alias', 'change_status', 'change_weight', 'question', 'question_content', 'report', 'export_excel' );
 
